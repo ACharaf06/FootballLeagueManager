84ebfff792a99fddc6e035f83afbb1c0e7602189
Charaf ACHIR <108930448+ACharaf06@users.noreply.github.com>
2024-01-30 19:15:55 +0100

    Update FootballLeagueManager.java

--- javabd/src/main/java/FootballLeagueManager.java
+++ javabd/src/main/java/FootballLeagueManager.java
@@ -1,11 +1,12 @@
 import com.mongodb.client.*;
 import com.mongodb.client.model.Aggregates;
 import com.mongodb.client.model.Filters;
-import com.mongodb.client.AggregateIterable;
-import com.mongodb.client.FindIterable;
-import com.mongodb.client.MongoCollection;
 import com.mongodb.client.model.Accumulators;
 import org.bson.Document;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.Arrays;
 import java.util.Date;
 import org.bson.types.ObjectId;
@@ -20,7 +21,7 @@ public class FootballLeagueManager {
         database = mongoClient.getDatabase(dbName);
     }
 
-    //First we define methodes for the Team Collection
+    //methodes for the Team Collection
 
     public void insertTeam(Document team) {
         MongoCollection<Document> collection = database.getCollection("Team");
@@ -51,8 +52,9 @@ public class FootballLeagueManager {
         }
     }
 
+    // effectue une jointure entre les deux col et sort par date
     public void listMatchesWithAggregate() {
-        MongoCollection<Document> collection = database.getCollection("matches");
+        MongoCollection<Document> collection = database.getCollection("Match");
         AggregateIterable<Document> matchDetails = collection.aggregate(
                 Arrays.asList(
                         Aggregates.lookup("teams", "teamId", "_id", "team_info"),
@@ -68,7 +70,7 @@ public class FootballLeagueManager {
     // Group teams by color method
     public void groupTeamsByColor() {
         try {
-            MongoCollection<Document> collection = database.getCollection("teams");
+            MongoCollection<Document> collection = database.getCollection("Team");
 
             AggregateIterable<Document> groupedResult = collection.aggregate(
                     Arrays.asList(
@@ -85,26 +87,12 @@ public class FootballLeagueManager {
         }
     }
 
-    //Sort teams by foundation year
-    public void sortTeamsByFoundationYear() {
-        try {
-            MongoCollection<Document> collection = database.getCollection("teams");
 
-            FindIterable<Document> sortedTeams = collection.find().sort(new Document("founded", 1)); // 1 for ascending order
-
-            for (Document team : sortedTeams) {
-                System.out.println(team.toJson());
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    //Now we define methodes for the Match collection
+    //methodes for the Match collection
 
     public void insertMatch(Document match) {
         try {
-            MongoCollection<Document> collection = database.getCollection("matches");
+            MongoCollection<Document> collection = database.getCollection("Match");
             collection.insertOne(match);
             System.out.println("Match inserted successfully.");
         } catch (Exception e) {
@@ -114,7 +102,7 @@ public class FootballLeagueManager {
 
     public Document findMatchById(String id) {
         try {
-            MongoCollection<Document> collection = database.getCollection("matches");
+            MongoCollection<Document> collection = database.getCollection("Match");
             return collection.find(Filters.eq("_id", new ObjectId(id))).first();
         } catch (Exception e) {
             e.printStackTrace();
@@ -124,7 +112,7 @@ public class FootballLeagueManager {
 
     public void updateMatch(String matchId, Document updateFields) {
         try {
-            MongoCollection<Document> collection = database.getCollection("matches");
+            MongoCollection<Document> collection = database.getCollection("Match");
             collection.updateOne(Filters.eq("_id", new ObjectId(matchId)), new Document("$set", updateFields));
             System.out.println("Match updated successfully.");
         } catch (Exception e) {
@@ -134,7 +122,7 @@ public class FootballLeagueManager {
 
     public void deleteMatch(String matchId) {
         try {
-            MongoCollection<Document> collection = database.getCollection("matches");
+            MongoCollection<Document> collection = database.getCollection("Match");
             collection.deleteOne(Filters.eq("_id", new ObjectId(matchId)));
             System.out.println("Match deleted successfully.");
         } catch (Exception e) {
@@ -146,22 +134,18 @@ public class FootballLeagueManager {
 
     public void listMatchesInStadium(String stadiumName) {
         try {
-            MongoCollection<Document> collection = database.getCollection("matches");
+            MongoCollection<Document> collection = database.getCollection("Match");
 
             // Query to find matches where the 'stadium.name' matches the specified name
-            FindIterable<Document> matches = collection.find(new Document("stadium.name", stadiumName));
+            FindIterable<Document> Match = collection.find(new Document("stadium.name", stadiumName));
 
-            for (Document match : matches) {
+            for (Document match : Match) {
                 System.out.println(match.toJson());
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-
-
-
-
 
 
 
@@ -178,37 +162,69 @@ public class FootballLeagueManager {
                 .append("players", Arrays.asList(
                         new Document("name", "Courtois")
                                 .append("firstName", "Thibaut")
-                                .append("birthdate", new Date(92, 4, 11)) // Date format is (year - 1900, month, day)
+                                .append("birthdate", Date.from(LocalDate.of(1987, 6, 24).atStartOfDay(ZoneId.systemDefault()).toInstant())) 
                                 .append("position", new Document("name", "Goalkeeper").append("number", 1)),
                         new Document("name", "Benzema")
                                 .append("firstName", "Karim")
-                                .append("birthdate", new Date(87, 11, 19)) // Date format is (year - 1900, month, day)
+                                .append("birthdate", Date.from(LocalDate.of(1987, 6, 24).atStartOfDay(ZoneId.systemDefault()).toInstant()))
                                 .append("position", new Document("name", "Forward").append("number", 9))
                 ));
 
-        manager.insertTeam(realMadrid);
+        Document barcelona = new Document("name", "FC Barcelona")
+                .append("colors", Arrays.asList("Blue", "Red"))
+                .append("players", Arrays.asList(
+                        new Document("name", "Ter Stegen")
+                                .append("firstName", "Marc-André")
+                                .append("birthdate", Date.from(LocalDate.of(1987, 6, 24).atStartOfDay(ZoneId.systemDefault()).toInstant())) 
+                                .append("position", new Document("name", "Goalkeeper").append("number", 1)),
+                        new Document("name", "Messi")
+                                .append("firstName", "Lionel")
+                                .append("birthdate", Date.from(LocalDate.of(1987, 6, 24).atStartOfDay(ZoneId.systemDefault()).toInstant())) 
+                                .append("position", new Document("name", "Forward").append("number", 10))
+                ));
 
-        // Insert a new match
-        Document newMatch = new Document("Team", Arrays.asList(new Document("goals", 3), new Document("goals", 2)))
-                .append("date", new Date()) 
-                .append("stadium", new Document("name", "Donor")
-                        .append("city", new Document("name", "Casablanca").append("country", "Maroc")));
+        manager.insertTeam(barcelona);
+
+    // creat and insert a new match
+        Document newMatch = new Document()
+        .append("Team", Arrays.asList(
+                new Document("teamId", "65a320c3480a9cddac5cb84d")
+                        .append("name", "Raja Casablanca")
+                        .append("goals", 3),
+                new Document("teamId", "65a3392164a296570cf3229a")
+                        .append("name", "Real Madrid")
+                        .append("goals", 2)))
+        .append("birthdate", Date.from(LocalDate.of(1987, 6, 24).atStartOfDay(ZoneId.systemDefault()).toInstant())) 
+        .append("stadium", new Document("name", "Donor")
+                .append("city", new Document("name", "Casablanca")
+                        .append("country", "Maroc")));
+
         manager.insertMatch(newMatch);
 
+
     // Find a match by ID
-        Document foundMatch = manager.findMatchById("matchIdHere");
+        Document foundMatch = manager.findMatchById("65a33ff4480a9cddac5cb86b");
         System.out.println(foundMatch.toJson());
 
     // Update a match
-        Document updateFields = new Document("stadium.name", "New Stadium Name");
-        manager.updateMatch("matchIdToUpdate", updateFields);
+        Document updateFields = new Document("Donor", "Camp Nou");
+        manager.updateMatch("65a33ff4480a9cddac5cb86b", updateFields);
 
     // Delete a match
-        manager.deleteMatch("matchIdToDelete");
+        manager.deleteMatch("65b910516a9bc7bcb066851b");
+    //group by
+        //manager.listMatchesInStadium("Stade de Port Taraport");
+
+    //liste toute les equipes
+        //manager.listAllTeams();
+
+    //group teams by color
+        //manager.groupTeamsByColor();
 
 
 
-        // Don't forget to close the connection when you're done
+
+
         manager.close();
     }
 }
